523898e49e79f89c4e0a42044a44cd0f5f72183f
bruno brunt <brunobrunt101@gmail.com>
2025-01-03 10:12:33 -0500

    Executing Shell Script

---
+++ .github/workflows/ascii-script.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+  # Install cowsay Program
+    sudo apt-get install cowsay y
+      
+  # Execute cowsay CMD
+   cowsay -f dragon "Run for cover, I am a DRAGON......RAWRRRR" >> dragon.txt
+       
+  # Test File Exists
+    cat dragon.txt | grep i "DRAGON"
+      
+  # Read File
+    cat dragon.txt
+      
+  # List Repo Files
+    ls ltra
