1d86e76aa920cde5166b2458ff00fb081629d03a
bruno brunt <brunobrunt101@gmail.com>
2025-01-03 12:34:06 -0500

    Made changes to the ascii.sh script

--- ascii-script.sh
+++ ascii-script.sh
@@ -1,15 +1,15 @@
 #!/bin/sh
   # Install cowsay Program
-    sudo apt-get install cowsay y
+    sudo apt-get install cowsay -y
       
   # Execute cowsay CMD
    cowsay -f dragon "Run for cover, I am a DRAGON......RAWRRRR" >> dragon.txt
        
   # Test File Exists
-    cat dragon.txt | grep i "DRAGON"
+    cat dragon.txt | grep -i "DRAGON"
       
   # Read File
     cat dragon.txt
       
   # List Repo Files
-    ls ltra
+    ls -ltra
